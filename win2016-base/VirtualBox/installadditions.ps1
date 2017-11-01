d0e1d0823f35a9257a303e2c4f5bc58212f1c9aa
Aleksandr SaPozhkov <alexander@sapozhkov.net>
2017-11-01 12:11:06 +0300

    fixing vbox additions installation

--- win2016-base/VirtualBox/installadditions.ps1
+++ win2016-base/VirtualBox/installadditions.ps1
@@ -1,9 +1,4 @@
-Copy-Item e:\cert c:\ -recurse
-$file = Get-ChildItem -Path c:\cert\vbox-sha1.cer
-$file | Import-Certificate -CertStoreLocation Cert:\LocalMachine\TrustedPublisher
-$file = Get-ChildItem -Path c:\cert\vbox-sha256.cer
-$file | Import-Certificate -CertStoreLocation Cert:\LocalMachine\TrustedPublisher
-$file = Get-ChildItem -Path c:\cert\vbox-sha256-r3.cer
-$file | Import-Certificate -CertStoreLocation Cert:\LocalMachine\TrustedPublisher
+e:\cert\VBoxCertUtil add-trusted-publisher e:\cert\vbox-sha1.cer --root e:\cert\vbox-sha1.cer
+e:\cert\VBoxCertUtil add-trusted-publisher e:\cert\vbox-sha256.cer --root e:\cert\vbox-sha256.cer
+e:\cert\VBoxCertUtil add-trusted-publisher e:\cert\vbox-sha256-r3.cer --root e:\cert\vbox-sha256-r3.cer
 e:\VBoxWindowsAdditions.exe /S
-sleep 60
